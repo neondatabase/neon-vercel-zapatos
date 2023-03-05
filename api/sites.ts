a0c46422e7db8eea5e1f92b23a72c0ac110ccf3f
George MacKerron <george@mackerron.co.uk>
2023-03-05 22:22:40 +0000

    Tweaks

--- api/sites.ts
+++ api/sites.ts
@@ -1,6 +1,7 @@
+// this file re-exports the request handler for Vercel Edge Functions
+
 import sites from '../src/sites';
 
-// this re-exports the request handler for Vercel Edge Functions
 export default (req: Request, ctx: any) => {
   const env = { DATABASE_URL: process.env.DATABASE_URL };
   return sites.fetch(req, env, ctx);  // note: passing process.env straight through oddly doesn't work
